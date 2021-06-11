b8b661ff49f285832c134a2da42fa5c771632c17
Sarkhan Jafarli <sarkhanjafarli12@gmail.com>
2021-06-12 03:11:43 +0400

    Final touches with printing and it's done

--- build/CMakeFiles/api-docs.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/api-docs"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/api-docs.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/cmake_directory_clean.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "api-docs/html"
-)
